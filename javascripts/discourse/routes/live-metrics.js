9c28e33ec9695966a124eb9793994a0310652584
heartbeatpleasure <248991679+heartbeatpleasure@users.noreply.github.com>
2026-07-18 03:23:22 +0200

    Heartrate - resolve translated browser page title

--- javascripts/discourse/routes/live-metrics.js
+++ javascripts/discourse/routes/live-metrics.js
@@ -1,7 +1,9 @@
 import DiscourseRoute from "discourse/routes/discourse";
+import { i18n } from "discourse-i18n";
+import { themePrefix } from "virtual:theme";
 
 export default class LiveMetricsRoute extends DiscourseRoute {
   titleToken() {
-    return "live_metrics.title";
+    return i18n(themePrefix("live_metrics.title"));
   }
 }
